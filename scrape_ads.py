04b1a158b28c1b01033534cdf8aa1dc1cda17e74
Michael <bjmmtin@gmail.com>
2024-10-16 12:32:36 -0400

    update the Data structure

--- scrape_ads.py
+++ scrape_ads.py
@@ -2,6 +2,13 @@ import asyncio
 from playwright.async_api import async_playwright, ElementHandle
 import pandas as pd  # Ensure pandas is installed
 
+class AdInfo:
+    def __init__(self):
+        self.basic_info = {}
+        self.about_advertiser = {}
+        self.beneficiary_and_payer = {}
+        self.european_union_transparency = {}
+
 async def get_parent(element: ElementHandle, nth: int):
     if element:
         current_element = element
@@ -32,7 +39,7 @@ async def scrape_ads_library(url: str):
             ads_data = []
 
             for a_block in all_ad_blocks:
-                ad_info = {}
+                ad_info = AdInfo()  # Create a new AdInfo object for each ad
                 ad_block = await get_parent(a_block, 6)
                 if ad_block:
                     see_detail_element = await ad_block.query_selector("text='See ad details'")  
@@ -50,44 +57,37 @@ async def scrape_ads_library(url: str):
                             library_id_element = await modal_element.query_selector("text='Platforms'")
 
                             basic_info_parent = await get_parent(library_id_element, 1)
-                            basic_info = {}
 
                             if basic_info_parent:
                                 next_sibling_handle = await basic_info_parent.evaluate_handle("el => el.nextElementSibling")
-
                                 if next_sibling_handle:
                                     sibling_text = await next_sibling_handle.evaluate("el => el.innerText")
-                                    basic_info['Size'] = sibling_text.strip()
+                                    ad_info.basic_info['Size'] = sibling_text.strip()
 
                                 prev_sibling_handle = await basic_info_parent.evaluate_handle("el => el.previousElementSibling")
-
                                 if prev_sibling_handle:
                                     sibling_text = await prev_sibling_handle.evaluate("el => el.innerText")
-                                    basic_info['Started'] = sibling_text.strip()
+                                    ad_info.basic_info['Started'] = sibling_text.strip()
 
                                     prev_sibling_handle = await prev_sibling_handle.evaluate_handle("el => el.previousElementSibling")
-
                                     if prev_sibling_handle:
                                         sibling_text = await prev_sibling_handle.evaluate("el => el.innerText")
-                                        basic_info['Library'] = sibling_text.strip()
+                                        ad_info.basic_info['Library'] = sibling_text.strip()
 
                             sponsored_element = await modal_element.query_selector("text='Sponsored'")
                             caption_element = await get_parent(sponsored_element, 9)
 
                             if caption_element:
                                 next_sibling_handle = await caption_element.evaluate_handle("el => el.nextElementSibling")
-
                                 if next_sibling_handle:
                                     sibling_text = await next_sibling_handle.evaluate("el => el.innerText")
-                                    basic_info['Short Summary'] = sibling_text.strip()
+                                    ad_info.basic_info['Short Summary'] = sibling_text.strip()
 
-                            ad_info['Basic info'] = basic_info
-                            print("Basic info:", ad_info['Basic info'])
+                            print("Basic info:", ad_info.basic_info)
                             
                             # Extracting about the advertiser
                             element = await modal_element.query_selector("text='About the advertiser'")
                             about_advertiser = await get_parent(element, 2)
-                            about_advertiser_info = {}
 
                             if about_advertiser:
                                 await about_advertiser.click()
@@ -97,10 +97,9 @@ async def scrape_ads_library(url: str):
 
                                 if more_info_element:
                                     next_sibling_handle = await more_info_element.evaluate_handle("el => el.nextElementSibling")
-
                                     if next_sibling_handle:
                                         sibling_text = await next_sibling_handle.evaluate("el => el.innerText")
-                                        about_advertiser_info['More info'] = sibling_text.strip()
+                                        ad_info.about_advertiser['More info'] = sibling_text.strip()
 
                                 about_advertiser_block = await get_parent(about_advertiser, 1)
                                 social_element = await about_advertiser_block.query_selector("text='Captions: For Talking Videos'")
@@ -108,45 +107,38 @@ async def scrape_ads_library(url: str):
 
                                 if social_element_parent:
                                     caption_text = await social_element_parent.inner_text()
-                                    about_advertiser_info['Caption info'] = caption_text.strip()
+                                    ad_info.about_advertiser['Caption info'] = caption_text.strip()
 
                                     next_sibling_handle = await social_element_parent.evaluate_handle("el => el.nextElementSibling")
-
                                     if next_sibling_handle:
                                         sibling_text = await next_sibling_handle.evaluate("el => el.innerText")
-                                        about_advertiser_info['facebook'] = sibling_text.strip()
+                                        ad_info.about_advertiser['facebook'] = sibling_text.strip()
 
                                         next_sibling_handle = await next_sibling_handle.evaluate_handle("el => el.nextElementSibling")
-
                                         if next_sibling_handle:
                                             sibling_text = await next_sibling_handle.evaluate("el => el.innerText")
-                                            about_advertiser_info['instagram'] = sibling_text.strip()
+                                            ad_info.about_advertiser['instagram'] = sibling_text.strip()
 
-                            ad_info['About the advertiser'] = about_advertiser_info
-                            print("About the advertiser:", ad_info['About the advertiser'])
+                            print("About the advertiser:", ad_info.about_advertiser)
                             
                             # Extracting beneficiary and payer
                             element = await modal_element.query_selector("text='Beneficiary and payer'")
                             beneficiary_and_payer = await get_parent(element, 2)
-                            beneficiary_and_payer_info = {}
-                            
+
                             if beneficiary_and_payer:
                                 await beneficiary_and_payer.click()
                                 await page.wait_for_timeout(500)
 
                                 next_sibling_handle = await beneficiary_and_payer.evaluate_handle("el => el.nextElementSibling")
-
                                 if next_sibling_handle:
                                     sibling_text = await next_sibling_handle.evaluate("el => el.innerText")
-                                    beneficiary_and_payer_info['Description'] = sibling_text.strip()
+                                    ad_info.beneficiary_and_payer['Description'] = sibling_text.strip()
 
-                            ad_info['Beneficiary and payer'] = beneficiary_and_payer_info
-                            print("Beneficiary and payer:", ad_info['Beneficiary and payer'])
+                            print("Beneficiary and payer:", ad_info.beneficiary_and_payer)
 
                             # Extracting European Union transparency
                             element = await modal_element.query_selector("text='European Union transparency'")
                             european_union_transparency = await get_parent(element, 2)
-                            european_union_transparency_info = {}
 
                             if european_union_transparency:
                                 await european_union_transparency.click()
@@ -156,12 +148,11 @@ async def scrape_ads_library(url: str):
                                 eu_ad_audience_info = {}
 
                                 prev_sibling_handle = await eu_ad_audience.evaluate_handle("el => el.previousElementSibling")
-
                                 if prev_sibling_handle:
                                     prev_sibling_handle = await prev_sibling_handle.evaluate_handle("el => el.previousElementSibling")
                                     if prev_sibling_handle:
                                         sibling_text = await prev_sibling_handle.evaluate("el => el.innerText")
-                                        european_union_transparency_info['Description'] = sibling_text.strip()                                     
+                                        ad_info.european_union_transparency['Description'] = sibling_text.strip()                                     
 
                                 next_sibling_handle = await eu_ad_audience.evaluate_handle("el => el.nextElementSibling")
                                 location_info = {}
@@ -170,13 +161,11 @@ async def scrape_ads_library(url: str):
 
                                 if next_sibling_handle:
                                     second_child_handle = await next_sibling_handle.evaluate_handle("el => el.children[1]")
-
                                     if second_child_handle:
                                         info_text = await second_child_handle.evaluate("el => el.innerText")
                                         location_info['Description'] = info_text.strip()
                                     
                                     third_child_handle = await next_sibling_handle.evaluate_handle("el => el.children[2]")
-
                                     if third_child_handle:
                                         info_text = await third_child_handle.evaluate("el => el.innerText")
                                         location_info['Table'] = info_text.strip()
@@ -187,13 +176,11 @@ async def scrape_ads_library(url: str):
                                     
                                     if next_sibling_handle:
                                         second_child_handle = await next_sibling_handle.evaluate_handle("el => el.children[1]")
-
                                         if second_child_handle:
                                             info_text = await second_child_handle.evaluate("el => el.innerText")
                                             age_info['Range'] = info_text.strip()
                                         
                                         third_child_handle = await next_sibling_handle.evaluate_handle("el => el.children[2]")
-
                                         if third_child_handle:
                                             info_text = await third_child_handle.evaluate("el => el.innerText")
                                             age_info['Description'] = info_text.strip()
@@ -204,20 +191,19 @@ async def scrape_ads_library(url: str):
                                     
                                         if next_sibling_handle:
                                             second_child_handle = await next_sibling_handle.evaluate_handle("el => el.children[1]")
-
                                             if second_child_handle:
                                                 info_text = await second_child_handle.evaluate("el => el.innerText")
                                                 gender_info['Value'] = info_text.strip()
                                             
                                             third_child_handle = await next_sibling_handle.evaluate_handle("el => el.children[2]")
-
                                             if third_child_handle:
                                                 info_text = await third_child_handle.evaluate("el => el.innerText")
                                                 gender_info['Description'] = info_text.strip()
 
                                             eu_ad_audience_info['Gender'] = gender_info
 
-                                    european_union_transparency_info['EU ad audience'] = eu_ad_audience_info
+                                    ad_info.european_union_transparency['EU ad audience'] = eu_ad_audience_info
+                                
                                 eu_ad_delivery = await modal_element.query_selector("text='EU ad delivery'")
                                 eu_ad_delivery_info = {}
 
@@ -226,13 +212,11 @@ async def scrape_ads_library(url: str):
                                 reach_table_info = {}
                                 if next_sibling_handle:
                                     second_child_handle = await next_sibling_handle.evaluate_handle("el => el.children[1]")
-
                                     if second_child_handle:
                                         info_text = await second_child_handle.evaluate("el => el.innerText")
                                         reach_info['Value'] = info_text.strip()
                                     
                                     third_child_handle = await next_sibling_handle.evaluate_handle("el => el.children[2]")
-
                                     if third_child_handle:
                                         info_text = await third_child_handle.evaluate("el => el.innerText")
                                         reach_info['Description'] = info_text.strip()
@@ -240,32 +224,28 @@ async def scrape_ads_library(url: str):
                                     eu_ad_delivery_info['Reach'] = reach_info
 
                                     next_sibling_handle = await next_sibling_handle.evaluate_handle("el => el.nextElementSibling")
-
                                     if next_sibling_handle:
                                         second_child_handle = await next_sibling_handle.evaluate_handle("el => el.children[1]")
-
                                         if second_child_handle:
                                             info_text = await second_child_handle.evaluate("el => el.innerText")
                                             reach_table_info['Description'] = info_text.strip()
                                         
                                         third_child_handle = await next_sibling_handle.evaluate_handle("el => el.children[2]")
-
                                         if third_child_handle:
                                             info_text = await third_child_handle.evaluate("el => el.innerText")
                                             reach_table_info['Table'] = info_text.strip()
 
                                         eu_ad_delivery_info['Reach by location, age and gender'] = reach_table_info
 
-                                    european_union_transparency_info['EU ad delivery'] = eu_ad_delivery_info
+                                    ad_info.european_union_transparency['EU ad delivery'] = eu_ad_delivery_info
 
-                            ad_info['European Union transparency'] = european_union_transparency_info
-                            print("European Union transparency", ad_info['European Union transparency'])
+                            print("European Union transparency", ad_info.european_union_transparency)
                                 
                         ads_data.append(ad_info)  # Append ad info to the list
                         await page.click("text='Close'")  # Close the modal after scraping
 
             # Save the ads data to different file formats
-            df = pd.DataFrame(ads_data)
+            df = pd.DataFrame([ad.__dict__ for ad in ads_data])  # Convert objects to dictionaries
 
             # Save as JSON
             df.to_json('scraped_ads_data.json', orient='records', lines=True)
@@ -278,7 +258,6 @@ async def scrape_ads_library(url: str):
             # Save as CSV
             df.to_csv('scraped_ads_data.csv', index=False)
             print("Data saved to scraped_ads_data.csv")
-
 
         except Exception as e:
             print(f"An error occurred: {e}")
